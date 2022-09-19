bcd96bddc74b366723fab27502b9d91333435c43
Nathaniel330 <nathaniel.valdez.soriano@gmail.com>
2022-09-19 17:55:25 +0800

    removed pages- careers and contact us

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const dotEnv = require("dotenv")
-const shoeRoutes = require("./routes/shoeRoutes")
+const routes = require("./routes/shoeRoutes")
 const app = express()
 const methodOverride = require('method-override');  
 
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-app.use("/", shoeRoutes)
+app.use("/", routes)
 
 
 //view engine
